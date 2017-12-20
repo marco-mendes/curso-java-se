517aad29324cc53f627758d0644da53679ac8a5a
Marco Mendes <marco.mendes@arkhi.com.br>
2017-12-20 12:23:38 -0200

    Exemplos de Java 8

---
+++ unidade09/src/main/java/exemplos/java8/forEach/OrdenacaoComLambda.java
@@ -0,0 +1,58 @@
+package exemplos.java8.forEach;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdenacaoComLambda {
+
+	public static void main(String[] args) {
+
+		List<Developer> listDevs = getDevelopers();
+
+		System.out.println("\nAntes da ordenacao");		
+		// Sem forEach (Java 7)
+		for (Developer developer : listDevs) {
+			System.out.println(developer);
+		}
+		
+		// Exemplo do uso do ForEach (Java 8)
+		System.out.println("\nAntes da ordenacao");		
+		listDevs.forEach(item -> System.out.println(item));
+		
+		// Exemplo do uso do ForEach com recurso chamado method reference
+		System.out.println("\nAntes da ordenacao");		
+		listDevs.forEach(System.out::println);
+
+	
+
+		System.out.println("\nDepois da ordenacao por idade\n");
+		// Lambda (ordenacao por idade)
+		listDevs.sort((Developer o1, Developer o2)->o1.getAge()-o2.getAge());
+		// Java 8 e lambda
+		listDevs.forEach((developer)->System.out.println(developer));
+
+		System.out.println("\nDepois da ordenacao por nome\n");
+		listDevs.sort((o1, o2)->o1.getName().compareTo(o2.getName()));
+		listDevs.forEach((developer)->System.out.println(developer));
+
+		System.out.println("\nDepois da ordenacao por salario\n");
+		listDevs.sort((o1, o2)->o1.getSalary().compareTo(o2.getSalary()));
+		listDevs.forEach((developer)->System.out.println(developer));
+		
+	}
+
+	private static List<Developer> getDevelopers() {
+
+		List<Developer> result = new ArrayList<Developer>();
+
+		result.add(new Developer("Maria", new BigDecimal("70000"), 33));
+		result.add(new Developer("Zenao", new BigDecimal("80000"), 20));
+		result.add(new Developer("Jose", new BigDecimal("100000"), 10));
+		result.add(new Developer("Abraao", new BigDecimal("170000"), 55));
+
+		return result;
+
+	}
+
+}
---
+++ unidade09/src/main/java/exemplos/java8/streams_filter_collect/ComJava8FiltrosAdicionais.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package exemplos.java8.streams_filter_collect;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Marco Mendes
+ * @since 2017
+ */
+public class ComJava8FiltrosAdicionais {
+    public static void main(String[] args) {
+
+        List<Person> persons = Arrays.asList(
+                new Person("Joao", 30),
+                new Person("Maria", 20),
+                new Person("Jose", 40)
+        );
+
+        Person maria = persons.stream()                         // Converte para Stream
+                .filter(x -> "Maria".equals(x.getName()))       // Apenas a Maria vai ser filtrada
+                .findAny()                                      // Se encontrou 'findAny' retorna found
+                .orElse(null);                                  // Se nao encontrou, retorna nada
+
+        System.out.println(maria);
+
+        Person carolina = persons.stream()
+                .filter(x -> "Carolina".equals(x.getName()))
+                .findAny()
+                .orElse(null);
+
+        System.out.println(carolina);
+        
+        persons = Arrays.asList(
+                new Person("Manoela", 30),
+                new Person("Bernardo", 20),
+                new Person("Flavio", 40)
+        );
+
+        Person manoela = persons.stream()
+                .filter((p) -> "Manoela".equals(p.getName()) && 30 == p.getAge())
+                .findAny()
+                .orElse(null);
+
+        System.out.println(manoela);
+
+        //Forma alternativa de escrita (mais verbosa)
+        manoela = persons.stream()
+                .filter(p -> {
+                    if ("Manoela".equals(p.getName()) && 30 == p.getAge()) {
+                        return true;
+                    }
+                    return false;
+                }).findAny()
+                .orElse(null);
+
+        System.out.println(manoela);
+        
+    }
+
+
+}
