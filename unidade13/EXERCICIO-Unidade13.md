8e2141bf35e94ba37786f49feb8fefafaa173cfe
Marco Mendes <marco.mendes@arkhi.com.br>
2017-12-29 00:34:37 -0200

    Criacao de projeto JUNIT5 com exemplo funcional de teste de unidade na classe ContaRegular

---
+++ unidade13/EXERCICIO-Unidade13.md
@@ -0,0 +1,10 @@
+Você deve fornecer testes positivos para os seguintes cenários:
+  - Adição de uma nova conta a um cliente VIP
+  - Obtencao do total de contas em uma conta VIP
+  - Obtencao saldo total de uma conta em uma conta VIP
+  - Obtencao da conta de maior saldo em uma conta VIP
+  - Obtencao do saldo medio de contas em uma conta VIP
+  - Obtencao de contas que estao no cheque especial em uma conta VIP
+  
+  Dica: Use o modelo fornecido no diretorio src/test/java
+  
--- unidade13/src/test/java/com/acme/cliente/ClienteRegularTest.java
+++ unidade13/src/test/java/com/acme/cliente/ClienteRegularTest.java
@@ -1,22 +1,80 @@
 package com.acme.cliente;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
+import com.acme.agencia.Agencia;
+import com.acme.excecoes.ContaInvalida;
+import com.acme.excecoes.LimiteChequeEspecialExcedido;
+import com.acme.excecoes.LimiteSaqueExcedido;
+import com.acme.excecoes.MovimentacaoInvalida;
+
+import junit.framework.Assert;
+
 @RunWith(JUnitPlatform.class)
 class ClienteRegularTest {
 
+	Agencia<ClienteVIP> agencia001;
+	ClienteRegular maria;
+
+	@BeforeAll
+	static void setup()  {
+		// Inicializacoes estaticas (executadas apenas uma unica vez
+	}
+
+	
+	@BeforeEach
+	void init() throws ContaInvalida {	
+		agencia001 = 
+				new Agencia<ClienteVIP>().
+				nome("Agencia Central").
+				endereco("Praça Sete de Setembro").
+				cep("30000").
+				telefone("(31)99999-9999");
+		
+		LocalDate dataNascimentoMaria = LocalDate.of(1974, Month.DECEMBER, 5);
+		maria = new ClienteRegular(agencia001, 
+				"Maria", "Rua das Flores", Optional.of("Rua das Flores Comercial"), 
+				"54321-6", dataNascimentoMaria, 500);
+		
+	}
+	
+	
 	@Test
-	void testCreditar() {
-		fail("Not yet implemented");
+	@DisplayName("Teste de credito para a Maria") 
+	void testCreditar() throws ContaInvalida, MovimentacaoInvalida, LimiteSaqueExcedido, LimiteChequeEspecialExcedido {
+		maria.creditar(400);		
+		assertEquals(900, maria.getSaldo());
 	}
 
 	@Test
-	void testDebitar() {
-		fail("Not yet implemented");
+	@DisplayName("Teste de debito para a Maria") 
+	void testDebitar() throws MovimentacaoInvalida, LimiteSaqueExcedido, LimiteChequeEspecialExcedido {
+		maria.debitar(400);		
+		assertEquals(100, maria.getSaldo());
+	}
+	
+	@AfterEach
+	void tearDown() {
+		// Finalizacao depois de um teste.
+	}
+	 
+	@AfterAll
+	static void done() {
+		// Finalizacao depois de todo o ciclo de teste. 
 	}
 
 }
